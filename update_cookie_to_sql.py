3de1e8e4d8a3d69fe0e6a8eba1a643a0df554818
暴风雨544 <1544007699@qq.com>
2024-06-03 22:04:06 +0800

    更新逆向的脚本 和新增传参Model:suno-v3,suno-v3.5

--- update_cookie_to_sql.py
+++ update_cookie_to_sql.py
@@ -9,7 +9,7 @@ SQL_password = os.getenv('SQL_password','')
 SQL_IP = os.getenv('SQL_IP','')
 SQL_dk = os.getenv('SQL_dk',3306)
 cookies = \
-    ['__cf_bm=z0fmOnL0WR4eqsWjjXs.EPWeswoBK0DALc34ApSbLVE-1717419211-1.0.1.1-yya_QPsxBpSbXjTNqCk.2GT2aH9R8fN6BaNHTlLJzAhEDpI2KaUHdPeWgBmMbOVe.gY1lHGDfl9AkDGEyUSDjA; _cfuvid=yJTQfwBENvtR8B6ycV8jqMIf.JE8TM1FLjXgh79tq1w-1717419211006-0.0.1.1-604800000; __client=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNsaWVudF8yaE4wUlNESllCb1VSRHhXR0dRclRYc0szaUYiLCJyb3RhdGluZ190b2tlbiI6ImZ4d2doY2wwbHhtajJneWkxNDl4d3czbmZrZnhsY3ZzcWRuM3lxbnYifQ.Dl_TmF3x3NsEiwcbcSjmxlYIHeEHmbIx5Qw6FPnWi9apduSxaBavMIaWMCgn5qw-nvlvdHgITQnK0OhlCMoNOach6MK0F9M2idnUsg8xW3AOJOkKqr1Cofj1vSz8qoZLwIABnwUDMY6ISGes_fa5vMoITptAulAsxnqvPtQO1PkzUXfqC3BsSO4S3R2pwK8sbNB-MjLjm_0c-4IEgWatAlsmRxmC5XDr1haACZVkDtOJ3LwUoneJaURj4d-R0Xr0ckoziUmvJ05mxhqOY0flBcj3PUVYod1uUs_WjlSdy2U0rZASJ-C8U1mP4oeKtn6KYmft8Va9VngnkFAFNQ-1nQ; __client_uat=1717419230; mp_26ced217328f4737497bd6ba6641ca1c_mixpanel=%7B%22distinct_id%22%3A%20%225a96919e-8046-4f17-9ffe-34cbbb9e93d6%22%2C%22%24device_id%22%3A%20%2218fde2971f4141d-04129a60e4379e-745d5774-154ac4-18fde2971f4141d%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D%2C%22__mpus%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpr%22%3A%20%5B%5D%2C%22__mpap%22%3A%20%5B%5D%2C%22%24user_id%22%3A%20%225a96919e-8046-4f17-9ffe-34cbbb9e93d6%22%7D']
+    ['你自己的cookie']
 
 async def fetch_limit_left(cookie,db_manager):
     song_gen = SongsGen(cookie)
