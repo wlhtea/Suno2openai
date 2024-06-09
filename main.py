82f6f483da41d38d315233b782dcefbf4022c377
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-09 14:16:44 +0800

    📌 fix get cookies bug

--- main.py
+++ main.py
@@ -32,17 +32,21 @@ async def refresh_cookies():
     try:
         logging.info(f"==========================================")
         logging.info("开始更新数据库里的 cookies.........")
-        cookies = await db_manager.get_cookies()
+        cookies = [item['cookie'] for item in await db_manager.get_cookies()]
         add_tasks = []
         for cookie in cookies:
+            logging.info(f"获取到cookie:{cookie}")
             add_tasks.append(fetch_limit_left(cookie))
         results = await asyncio.gather(*add_tasks, return_exceptions=True)
         success_count = sum(1 for result in results if result is True)
         fail_count = len(cookies) - success_count
 
         logging.info({"message": "Cookies 更新成功。", "成功数量": success_count, "失败数量": fail_count})
+
+    except HTTPException as http_exc:
+        raise http_exc
     except Exception as e:
-        logging.error({"错误": str(e)})
+        raise {"error": e}
 
 
 @asynccontextmanager
@@ -459,6 +463,7 @@ async def add_cookies(data: schemas.Cookies, authorization: str = Header(...)):
         success_count = sum(1 for result in results if result is True)
         fail_count = len(cookies) - success_count
 
+        logging.info({"message": "Cookies 更新成功。", "成功数量": success_count, "失败数量": fail_count})
         return JSONResponse(
             content={"message": "Cookies add successfully.", "success_count": success_count, "fail_count": fail_count})
     except HTTPException as http_exc:
@@ -491,17 +496,20 @@ async def get_last_user_message(data: schemas.Cookies, authorization: str = Head
 
 # 请求刷新cookies
 @app.get(f"{cookies_prefix}/refresh/cookies")
-async def get_refresh_cookies():
+async def get_refresh_cookies(authorization: str = Header(...)):
     try:
+        await verify_auth_header(authorization)
         logging.info(f"==========================================")
         logging.info("开始更新数据库里的 cookies.........")
-        cookies = await db_manager.get_cookies()
+        cookies = [item['cookie'] for item in await db_manager.get_cookies()]
         add_tasks = []
         for cookie in cookies:
+            logging.info(f"获取到cookie:{cookie}")
             add_tasks.append(fetch_limit_left(cookie))
         results = await asyncio.gather(*add_tasks, return_exceptions=True)
         success_count = sum(1 for result in results if result is True)
         fail_count = len(cookies) - success_count
+
         logging.info({"message": "Cookies 更新成功。", "成功数量": success_count, "失败数量": fail_count})
         return JSONResponse(
             content={"message": "Cookies add successfully.", "success_count": success_count, "fail_count": fail_count})
