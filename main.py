4c4b3b0d84de2f7f1f6db1deaac2568882c735b9
暴风雨544 <1544007699@qq.com>
2024-06-10 19:57:03 +0800

    test

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from utils import generate_music, get_feed
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+
 app = FastAPI()
 
 app.add_middleware(
@@ -34,7 +34,7 @@ import random
 import string
 import time
 from sql_uilts import DatabaseManager
-
+load_dotenv()
 BASE_URL = os.getenv('BASE_URL', 'https://studio-api.suno.ai')
 SESSION_ID = os.getenv('SESSION_ID')
 username_name = os.getenv('USER_Name','')
@@ -43,10 +43,10 @@ SQL_password = os.getenv('SQL_password', '')
 SQL_IP = os.getenv('SQL_IP', '')
 SQL_dk = os.getenv('SQL_dk', 3306)
 
-db_manager = DatabaseManager(SQL_IP, int(SQL_dk), username_name, SQL_password, SQL_name)
-
+print(BASE_URL,SESSION_ID,username_name,SQL_name,SQL_password,SQL_IP,SQL_dk)
 @app.on_event("startup")
 async def on_startup():
+    db_manager = DatabaseManager(SQL_IP, int(SQL_dk), username_name, SQL_password, SQL_name)
     await db_manager.create_database_and_table()
 
 def generate_random_string_async(length):
@@ -95,16 +95,18 @@ def get_clips_ids(response: json):
         raise ValueError("Invalid JSON response")
 
 
-async def get_token():
+async def get_token(db_manager):
     cookieSelected = await db_manager.get_token()
     return cookieSelected
 
 
-async def Delelet_Songid(songid):
+async def Delelet_Songid(songid,db_manager):
     return await db_manager.delete_song_ids(songid)
 
 
 async def generate_data(chat_user_message, chat_id, timeStamp, ModelVersion, tags=None, title=None, continue_at=None, continue_clip_id=None):
+    db_manager = DatabaseManager(SQL_IP, int(SQL_dk), username_name, SQL_password, SQL_name)
+    print(SQL_IP,username_name)
     while True:
         try:
             await db_manager.create_pool()
