921f947d4ae4a80aae1e7316e0067ae7810c4fbd
暴风雨544 <1544007699@qq.com>
2024-06-10 20:53:40 +0800

    歌词bug

--- main.py
+++ main.py
@@ -160,9 +160,9 @@ async def generate_data(chat_user_message, chat_id, timeStamp, ModelVersion, tag
         await db_manager.decrement_cookie_count(cookie)
 
         for clip_id in clip_ids:
-            attempts = 5
+            attempts = 2
             while True:
-                if attempts // 5 == 0:
+                if attempts // 2 == 0:
                     token, sid = SongsGen(cookie)._get_auth_token(w=1)
                 now_data = await get_feed(ids=clip_id, token=token)
                 try:
