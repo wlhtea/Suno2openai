1696f87f19da5512be80b61ad1ebe13738ea3f1d
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-13 13:42:08 +0800

    feat 提高并发速度

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def cron_refresh_cookies():
         logging.info(f"==========================================")
         logging.info("开始更新数据库里的 cookies.........")
         cookies = [item['cookie'] for item in await db_manager.get_cookies()]
-        semaphore = asyncio.Semaphore(5)
+        semaphore = asyncio.Semaphore(20)
         add_tasks = []
 
         async def refresh_cookie(simple_cookie):
@@ -511,7 +511,7 @@ async def add_cookies(data: schemas.Cookies, authorization: str = Header(...)):
         if not cookies:
             raise HTTPException(status_code=400, detail="Cookies 列表为空")
         
-        semaphore = asyncio.Semaphore(5)
+        semaphore = asyncio.Semaphore(20)
         add_tasks = []
 
         async def add_cookie(simple_cookie):
@@ -568,7 +568,7 @@ async def refresh_cookies(authorization: str = Header(...)):
         logging.info(f"==========================================")
         logging.info("开始更新数据库里的 cookies.........")
         cookies = [item['cookie'] for item in await db_manager.get_cookies()]
-        semaphore = asyncio.Semaphore(5)
+        semaphore = asyncio.Semaphore(20)
         add_tasks = []
 
         async def refresh_cookie(simple_cookie):
