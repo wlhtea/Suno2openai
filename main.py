f54396709c9e6129a47f77cbbb2f56cdb97480aa
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-16 14:28:44 +0800

    📌 fix format

--- main.py
+++ main.py
@@ -217,6 +217,7 @@ async def generate_data(chat_user_message, chat_id, timeStamp, ModelVersion, tag
                         continue_clip_id=None):
     for try_count in range(retries):
         cookie = None
+        song_gen = None
         try:
             for attempt in range(retries):
                 try:
@@ -246,7 +247,7 @@ async def generate_data(chat_user_message, chat_id, timeStamp, ModelVersion, tag
             _return_video_url = False
             _return_audio_url = False
             _return_Forever_url = False
-            token, sid = SongsGen(cookie).get_auth_token(w=1)
+            token, sid = song_gen.get_auth_token(w=1)
 
             suno_auth.set_session_id(sid)
             suno_auth.load_cookie(cookie)
@@ -284,7 +285,7 @@ async def generate_data(chat_user_message, chat_id, timeStamp, ModelVersion, tag
             song_id_2 = clip_ids[1]
             # await db_manager.update_song_ids_by_cookie(cookie, song_id_1, song_id_2)
 
-            yield f"""data:""" + ' ' + f"""{json.dumps({"id": f"chatcmpl-{chat_id}", "object": "chat.completion.chunk", "model": "suno-v3", "created": timeStamp, "choices": [{"index": 0, "delta": {"role": "assistant", "content": ""}, "finish_reason": None}]})}\n\n"""
+            yield f"""data:""" + ' ' + f"""{json.dumps({"id": f"chatcmpl-{chat_id}", "object": "chat.completion.chunk", "model": "suno-v3", "created": timeStamp, "choices": [{"index": 0, "delta": {"role": "assistant", "content": f"\n```text\n{"{prompt:" + {chat_user_message} + "}"}\n```"}, "finish_reason": None}]})}\n\n"""
             for clip_id in clip_ids:
                 count = 0
                 while True:
