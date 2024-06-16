e0f029f48cd07caeedc211d96c0acb841bebffc5
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-16 14:43:47 +0800

    📌 fix format

--- main.py
+++ main.py
@@ -284,7 +284,7 @@ async def generate_data(chat_user_message, chat_id, timeStamp, ModelVersion, tag
             song_id_2 = clip_ids[1]
             # await db_manager.update_song_ids_by_cookie(cookie, song_id_1, song_id_2)
 
-            yield f"""data: """ + ' ' + f"""{json.dumps({"id": f"chatcmpl-{chat_id}", "object": "chat.completion.chunk", "model": ModelVersion, "created": timeStamp, "choices": [{"index": 0, "delta": {"role": "assistant", "content": f"""```text\n{{{{prompt:{chat_user_message}}}}}\n```"""}, "finish_reason": None}]})}\n\n"""
+            yield f"""data: """ + ' ' + f"""{json.dumps({"id": f"chatcmpl-{chat_id}", "object": "chat.completion.chunk", "model": ModelVersion, "created": timeStamp, "choices": [{"index": 0, "delta": {"role": "assistant", "content": f"""```text\\n{{{{prompt:{chat_user_message}}}}}\\n```"""}, "finish_reason": None}]})}\n\n"""
             for clip_id in clip_ids:
                 count = 0
                 while True:
