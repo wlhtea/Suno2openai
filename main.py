3ad6ebcc6b69ba68972f1b6715d01153fb6c38ce
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-17 15:42:10 +0800

    💄 优化报错处理

--- main.py
+++ main.py
@@ -252,14 +252,13 @@ async def generate_data(chat_user_message, chat_id, timeStamp, ModelVersion, tag
                         if remaining_count == -1:
                             await db_manager.delete_cookies(cookie)
                             raise RuntimeError("该账号剩余次数为 -1，无法使用")
-                        logging.info(f"请求第 {attempt + 1} 次获取到cookie:{cookie}")
                         break
                 except Exception as e:
-                    logging.error(f"第 {attempt + 1} 次尝试获取cookie失败，错误为：{str(e)}")
+                    logging.error(f"在请求重试{try_count}次中，第 {attempt + 1} 次尝试获取cookie失败，错误为：{str(e)}")
                     if attempt < retries - 1:
                         continue
                     else:
-                        raise RuntimeError(f"获取cookie失败cookie发生异常: {e}")
+                        raise RuntimeError(f"在请求重试{try_count}次中，获取cookie全部失败，cookie发生异常: {e}")
 
             _return_ids = False
             _return_tags = False
