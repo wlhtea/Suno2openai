193c67a695bff8280a2d320623f9ac11e4de4322
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-20 12:48:22 +0800

    💄 适配更多的mysql

--- data/message.py
+++ data/message.py
@@ -32,8 +32,9 @@ async def generate_data(db_manager, chat_user_message, chat_id, timeStamp, Model
                 # 测试并发集
                 # yield f"""data:""" + ' ' + f"""{json.dumps({"id": f"chatcmpl-{chat_id}", "object":
                 # "chat.completion.chunk", "model": ModelVersion, "created": timeStamp, "choices": [{"index": 0,
-                # "delta": {"content": str(cookie)}, "finish_reason": None}]})}\n\n""" yield f"""data:""" + ' ' +
-                # f"""[DONE]\n\n""" return
+                # "delta": {"content": str(cookie)}, "finish_reason": None}]})}\n\n"""
+                # yield f"""data:""" + ' ' + f"""[DONE]\n\n"""
+                # return
 
             _return_ids = False
             _return_tags = False
