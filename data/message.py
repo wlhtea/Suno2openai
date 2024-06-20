9c1ca5417d8ef6aaae23b7c80fee12a0490fb67b
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-20 09:51:24 +0800

    🤗 fix bug

--- data/message.py
+++ data/message.py
@@ -23,14 +23,17 @@ async def generate_data(db_manager, chat_user_message, chat_id, timeStamp, Model
             if cookie is None:
                 raise RuntimeError("没有可用的cookie")
             else:
-                # song_gen = SongsGen(cookie)
-                # remaining_count = song_gen.get_limit_left()
-                # if remaining_count == -1:
-                #     await db_manager.delete_cookies(cookie)
-                #     raise RuntimeError("该账号剩余次数为 -1，无法使用")
-                yield f"""data:""" + ' ' + f"""{json.dumps({"id": f"chatcmpl-{chat_id}", "object": "chat.completion.chunk", "model": ModelVersion, "created": timeStamp, "choices": [{"index": 0, "delta": {"content": str(cookie)}, "finish_reason": None}]})}\n\n"""
-                yield f"""data:""" + ' ' + f"""[DONE]\n\n"""
-                return
+                song_gen = SongsGen(cookie)
+                remaining_count = song_gen.get_limit_left()
+                if remaining_count == -1:
+                    await db_manager.delete_cookies(cookie)
+                    raise RuntimeError("该账号剩余次数为 -1，无法使用")
+
+                # 测试并发集
+                # yield f"""data:""" + ' ' + f"""{json.dumps({"id": f"chatcmpl-{chat_id}", "object":
+                # "chat.completion.chunk", "model": ModelVersion, "created": timeStamp, "choices": [{"index": 0,
+                # "delta": {"content": str(cookie)}, "finish_reason": None}]})}\n\n""" yield f"""data:""" + ' ' +
+                # f"""[DONE]\n\n""" return
 
             _return_ids = False
             _return_tags = False
