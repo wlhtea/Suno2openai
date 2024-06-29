fe91742ed213f1192896e1148eddd7fa36c07493
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-30 00:25:57 +0800

    Revert "Update message.py"
    This reverts commit 9e8a232de35822eec82b54210f50384444575a2f.

--- data/message.py
+++ data/message.py
@@ -266,7 +266,6 @@ async def generate_data(start_time, db_manager, chat_user_message, chat_id,
                     loop = asyncio.get_event_loop()
                     if loop.is_running():
                         loop.create_task(end_chat(cookie, db_manager, remaining_count))
-                        logger.info("loop.create_task")
                         break
                     else:
                         await loop.run_until_complete(end_chat(cookie, db_manager, remaining_count))
