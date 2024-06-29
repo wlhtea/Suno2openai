056e694fc5ccbf788c806fa9a8d8a4e7e2e69194
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-06-30 00:18:42 +0800

    Update message.py

--- data/message.py
+++ data/message.py
@@ -265,9 +265,7 @@ async def generate_data(start_time, db_manager, chat_user_message, chat_id,
                 try:
                     loop = asyncio.get_event_loop()
                     if loop.is_running():
-                        task = loop.create_task(end_chat(cookie, db_manager, remaining_count))
-                        while not task.done():
-                            logger.info("检测任务完成中。。。。。。")
+                        loop.create_task(end_chat(cookie, db_manager, remaining_count))
                         break
                     else:
                         await loop.run_until_complete(end_chat(cookie, db_manager, remaining_count))
